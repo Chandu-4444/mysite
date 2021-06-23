fce700e8f46a14c03e15e4e0d78808371727bd22
Chandu-4444 <chandrakiran.g19@iiits.in>
2021-06-23 11:35:27 +0530

    Add comments, email backend

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+
 
 
 # # Function Based view
@@ -35,6 +38,35 @@ def post_detail(request, year, month, day, post):
     publish__month=month,
     publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = Comment.objects.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
 
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments':comments, 'new_comment':new_comment, 'comment_form':comment_form})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url()) # build_absolute_uri() attaches the full url, http://127.0.0.1:8000.....
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments :{cd['comment']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+        
+    return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT=1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
